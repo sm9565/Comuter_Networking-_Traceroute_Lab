60da9e418f08c7cffb7d481ed20da251581ad98f
sm9565 <70976372+sm9565@users.noreply.github.com>
2020-11-02 15:27:41 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -126,7 +126,6 @@ def get_route(hostname):
                 # Fill in end
                 try:  # try to fetch the hostname
                     # Fill in start
-                    hostname =gethostbyaddr(destAddr)
                     tracelist1.append(str(hostname))
                     # Fill in end
                 except herror:  # if the host does not provide a hostname
