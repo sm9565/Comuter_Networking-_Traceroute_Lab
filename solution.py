cbb31bd5e2e2e18c73f52c84ca7858a916c2547f
sm9565 <70976372+sm9565@users.noreply.github.com>
2020-11-02 16:00:33 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -139,7 +139,7 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
                     # You should add your responses to your lists here
-                    tracelist1.append(" %d %.0fms %s" % (ttl, (timeReceived - timeSent) * 1000, addr[0]))
+                    tracelist1.append(" %d %.0fms %s %s" % (ttl, (timeReceived - timeSent) * 1000, addr[0], hostname))
                     tracelist2.append(tracelist1)
                     # Fill in end
                 elif types == 3:
@@ -147,7 +147,7 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
                     # You should add your responses to your lists here
-                    tracelist1.append(" %d %.0fms %s" % (ttl, (timeReceived - timeSent) * 1000, addr[0]))
+                    tracelist1.append(" %d %.0fms %s %s" % (ttl, (timeReceived - timeSent) * 1000, addr[0], hostname))
                     tracelist2.append(tracelist1)
                     # Fill in end
                 elif types == 0:
@@ -155,7 +155,7 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
                     # You should add your responses to your lists here and return your list if your destination IP is met
-                    tracelist1.append(" %d %.0fms %s" % (ttl, (timeReceived - timeSent) * 1000, addr[0]))
+                    tracelist1.append(" %d %.0fms %s %s" % (ttl, (timeReceived - timeSent) * 1000, addr[0], hostname))
                     tracelist2.append(tracelist1)
                     return tracelist2
                     # Fill in end
